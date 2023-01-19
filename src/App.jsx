b4deec928e4fbfeb2107c9cb0af91e9fb3728440
Zahirw <wakidzahir@gmail.com>
2023-01-19 17:40:34 +0700

    create label task component

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Label from './component/label/Label';
+
+function App() {
+  return (
+    <div className="App">
+      <Label 
+        type="task1"
+      />
+    </div>
+  );
+}
+
+export default App;
