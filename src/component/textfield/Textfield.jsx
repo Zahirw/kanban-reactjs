ffbabf4f9aa0bace88c8a3c621d430b48c050e5e
Zahirw <wakidzahir@gmail.com>
2023-01-22 07:58:10 +0700

    add unit test textfiled input

--- src/component/textfield/Textfield.jsx
+++ src/component/textfield/Textfield.jsx
@@ -5,8 +5,8 @@ import styles from './textfield.module.scss'
 const Textfield = (props) => {
   return (
     <div className={styles.textfield}>
-      <label className={styles.label_text}>{props.label}</label>
-      <input className={props.type ? styles.progress : ''} type="text" placeholder={props.placeholder} onChange={props.onChange} defaultValue={props.value}/>
+      <label className={styles.label_text} data-cy='label-input'>{props.label}</label>
+      <input className={props.type ? styles.progress : ''} type="text" placeholder={props.placeholder} onChange={props.onChange} defaultValue={props.value} data-cy='input'/>
     </div>
   )
 }
